561960379f2e12d9162a9dd82537349ab7547686
urwatariq50-eng <urwatariq50@gmail.com>
2026-07-14 17:27:26 +0500

    Add back to top button functionality

--- js/script.js
+++ js/script.js
@@ -166,3 +166,23 @@ document.getElementById("minutes").innerHTML = minutes;
 document.getElementById("seconds").innerHTML = seconds;
 
 },1000);
+// BACK TO TOP BUTTON
+
+const topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+
+if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+topBtn.style.display = "block";
+} else {
+topBtn.style.display = "none";
+}
+
+};
+
+topBtn.onclick = function () {
+window.scrollTo({
+top: 0,
+behavior: "smooth"
+});
+};
