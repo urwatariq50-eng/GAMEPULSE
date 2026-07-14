8187189376f9b60bbca0cf3a4c6daedf750363f2
urwatariq50-eng <urwatariq50@gmail.com>
2026-07-14 23:32:32 +0500

    Remove GTA VI countdown code
    Removed the GTA VI countdown functionality from script.js.

--- js/script.js
+++ js/script.js
@@ -142,30 +142,7 @@ document.getElementById("loader").classList.add("hide");
 },2000);
 
 });
-// GTA VI Countdown
 
-const releaseDate = new Date("May 26, 2026 00:00:00").getTime();
-
-setInterval(function(){
-
-const now = new Date().getTime();
-
-const distance = releaseDate - now;
-
-const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-
-const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-
-const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-document.getElementById("days").innerHTML = days;
-document.getElementById("hours").innerHTML = hours;
-document.getElementById("minutes").innerHTML = minutes;
-document.getElementById("seconds").innerHTML = seconds;
-
-},1000);
 // BACK TO TOP BUTTON
 
 const topBtn = document.getElementById("topBtn");
