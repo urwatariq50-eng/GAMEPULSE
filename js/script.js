c79f2f4e4f8b03abb3bed5d2ed9cf131b989b55e
urwatariq50-eng <urwatariq50@gmail.com>
2026-07-15 01:37:49 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -189,8 +189,7 @@ lightbox.onclick = (e) => {
 };
 // GTA VI COUNTDOWN
 
-const releaseDate = new Date("May 26, 2026 00:00:00").getTime();
-
+const releaseDate = new Date("December 31, 2026 23:59:59").getTime();
 function updateCountdown() {
 
 const days = document.getElementById("days");
